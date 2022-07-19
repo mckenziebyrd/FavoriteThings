bc85065bdf4bd343bb21037963c0a3860545a8ea
McKenzie Byrd <mckenziebyrd@me.com>
2022-07-18 20:21:10 -0500

    Add project files.

---
+++ FavoriteThings/Dogs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteThings
+{
+    internal class Dogs
+    {
+        public string Breed { get; set; }
+
+        public string Name { get; set; }
+        
+        public int Quantity { get; set; }
+
+
+    }
+}
---
+++ FavoriteThings/Ramen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteThings
+{
+    internal class Ramen
+    {
+        public string Resturant { get; set; }
+
+        public string Type { get; set; }
+
+        public string Toppings { get; set; }
+
+    }
+}
